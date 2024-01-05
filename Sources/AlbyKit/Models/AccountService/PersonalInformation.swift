0bafe1d35a3714a3e6342a7636e7a21f2471808a
Thomas Rademaker <thomas@sparrowtek.com>
2024-01-05 16:04:48 -0500

    some properties need to be nil

--- Sources/AlbyKit/Models/AccountService/PersonalInformation.swift
+++ Sources/AlbyKit/Models/AccountService/PersonalInformation.swift
@@ -2,11 +2,11 @@
 public struct PersonalInformation: Codable, Sendable {
     public let identifier: String
     public let email: String
-    public let name: String
-    public let avatar: String
+    public let name: String?
+    public let avatar: String?
     public let keysendCustomKey: String
     public let keysendCustomValue: String
     public let keysendPubkey: String
     public let lightningAddress: String
-    public let nostrPubkey: String
+    public let nostrPubkey: String?
 }
