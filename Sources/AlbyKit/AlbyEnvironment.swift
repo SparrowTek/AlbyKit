597df4ee6a05d1e9b6881044e96a6e808840b5d2
Thomas Rademaker <thomas@sparrowtek.com>
2024-01-09 11:18:15 -0500

    fix for refresh token not updating automatically:

--- Sources/AlbyKit/AlbyEnvironment.swift
+++ Sources/AlbyKit/AlbyEnvironment.swift
@@ -19,6 +19,7 @@ class AlbyEnvironment {
     let routerDelegate = AlbyRouterDelegate()
     let authManager = AuthManager()
     weak var delegate: AlbyKitDelegate?
+    var tokenRefreshRequired = false
     
     private init() {}
     
--- Sources/AlbyKit/Networking/AuthManager.swift
+++ Sources/AlbyKit/Networking/AuthManager.swift
@@ -2,19 +2,13 @@ import Foundation
 
 actor AuthManager {
     private var refreshTask: Task<Void, Error>?
-    var tokenRefreshRequired = false
-    
-    func triggerTokenRefreshRequired() {
-        guard tokenRefreshRequired else { return }
-        tokenRefreshRequired = true
-    }
     
     func validateToken() async throws {
         if let refreshTask {
             try await refreshTask.value
             self.refreshTask = nil
-            tokenRefreshRequired = false
-        } else if tokenRefreshRequired {
+            AlbyEnvironment.current.tokenRefreshRequired = false
+        } else if AlbyEnvironment.current.tokenRefreshRequired {
             try await refreshToken()
         } else if let token = Storage.retrieve(AlbyEnvironment.Constants.token, from: .documents, as: TokenMetadata.self) {
             let tokenExpirationDate = token.createdAt.addingTimeInterval(TimeInterval(token.expiresIn))
@@ -41,7 +35,7 @@ actor AuthManager {
             refreshTask = task
             try await task.value
             refreshTask = nil
-            tokenRefreshRequired = false
+            AlbyEnvironment.current.tokenRefreshRequired = false
         }
     }
 }
