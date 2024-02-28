41dc490131c1d8ee33211588725b0fd46fda94a0
Thomas Rademaker <thomas@sparrowtek.com>
2024-02-28 15:54:22 -0500

    update model

--- Sources/AlbyKit/Models/InvoicesService/CreatedInvoice.swift
+++ Sources/AlbyKit/Models/InvoicesService/CreatedInvoice.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 public struct CreatedInvoice: Codable, Hashable, Sendable {
-    public let expires_at: Date
+    public let amount: Int
+    public let expiresAt: Date
     public let paymentHash: String
     public let paymentRequest: String
 }
