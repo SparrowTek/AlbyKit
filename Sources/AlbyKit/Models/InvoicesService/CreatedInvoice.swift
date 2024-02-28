d9f426f86a26ef2f95a9dc8d989d5e05813523a6
Thomas Rademaker <thomas@sparrowtek.com>
2024-02-28 15:46:43 -0500

    make struct hashable

--- Sources/AlbyKit/Models/InvoicesService/CreatedInvoice.swift
+++ Sources/AlbyKit/Models/InvoicesService/CreatedInvoice.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct CreatedInvoice: Codable, Sendable {
+public struct CreatedInvoice: Codable, Hashable, Sendable {
     public let expires_at: Date
     public let paymentHash: String
     public let paymentRequest: String
