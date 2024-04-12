c26fb846dfe046520783e29eba92f5c325a42b96
Thomas Rademaker <thomas@sparrowtek.com>
2024-04-12 12:40:47 -0400

    missed some properties to make optional

--- Sources/AlbyKit/Models/PaymentsService/Bolt11Payment.swift
+++ Sources/AlbyKit/Models/PaymentsService/Bolt11Payment.swift
@@ -1,10 +1,11 @@
 
 public struct Bolt11Payment: AlbyCodable, Equatable, Sendable {
-    public let amount: Int
-    public let descriptionHash: String
-    public let destination: String
-    public let fee: Int
-    public let paymentHash: String
-    public let paymentPreimage: String
-    public let paymentRequest: String
+    public let amount: Int?
+    public let descriptionHash: String?
+    public let description: String?
+    public let destination: String?
+    public let fee: Int?
+    public let paymentHash: String?
+    public let paymentPreimage: String?
+    public let paymentRequest: String?
 }
--- Sources/AlbyKit/Models/PaymentsService/KeysendPayment.swift
+++ Sources/AlbyKit/Models/PaymentsService/KeysendPayment.swift
@@ -1,13 +1,13 @@
 
 public struct KeysendPayment: AlbyCodable, Equatable, Sendable {
-    public let amount: Int
-    public let description: String
-    public let descriptionHash: String
-    public let destination: String
-    public let fee: Int
+    public let amount: Int?
+    public let description: String?
+    public let descriptionHash: String?
+    public let destination: String?
+    public let fee: Int?
     public let customRecords: [String : String]?
-    public let paymentHash: String
-    public let paymentPreimage: String
+    public let paymentHash: String?
+    public let paymentPreimage: String?
 }
 
 public struct MultiKeysendPayment: AlbyCodable, Sendable {
@@ -16,11 +16,11 @@ public struct MultiKeysendPayment: AlbyCodable, Sendable {
 }
 
 public struct KeysendPayments: AlbyCodable, Sendable {
-    public let keysends: [MultiKeysendPayment]
+    public let keysends: [MultiKeysendPayment]?
 }
 
 public struct KeysendPaymentError: AlbyCodable, Sendable {
-    public let code: Int
-    public let error: Bool
-    public let message: String
+    public let code: Int?
+    public let error: Bool?
+    public let message: String?
 }
