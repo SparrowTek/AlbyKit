617f6277d663f03ed3af1aee336b39c2d0171e33
Thomas Rademaker <thomas@radmakr.com>
2024-07-29 17:25:54 -0400

    use breeze liquid SDK to create ParsedBolt11Invoice

--- Package.swift
+++ Package.swift
@@ -16,9 +16,15 @@ let package = Package(
             name: "AlbyKit",
             targets: ["AlbyKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/breez/breez-sdk-liquid-swift.git", from: "0.2.0"),
+    ],
     targets: [
         .target(
             name: "AlbyKit",
+            dependencies: [
+                .product(name: "BreezSDKLiquid", package: "breez-sdk-liquid-swift"),
+            ],
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
             ]
--- Sources/AlbyKit/Models/InvoicesService/Bolt11Invoice.swift
+++ Sources/AlbyKit/Models/InvoicesService/Bolt11Invoice.swift
@@ -1,4 +1,5 @@
 import Foundation
+import BreezSDKLiquid
 
 public struct Bolt11Invoice: AlbyCodable, Sendable {
     public let currency: String?
@@ -12,4 +13,29 @@ public struct Bolt11Invoice: AlbyCodable, Sendable {
     public let amount: Int?
     public let payeeAlias: String?
     public let routeHintAliases: [String]? // TODO: is string array the correct type?
+}
+
+public struct ParsedBolt11Invoice {
+    var amount: UInt64?
+    
+    init?(invoice: String) {
+        amount = getSatsFromInvoice(bolt11: invoice)
+    }
+    
+    private func getSatsFromInvoice(bolt11: String) -> UInt64? {
+        do {
+            // Parse the Bolt 11 invoice using Breez SDK
+            let parsedInvoice = try BreezSDKLiquid.parseInvoice(input: bolt11)
+            
+            // Extract the amount in satoshis
+            if let amountMsat = parsedInvoice.amountMsat {
+                let sats = amountMsat / 1000
+                return sats
+            } else {
+                return nil
+            }
+        } catch {
+            return nil
+        }
+    }
 }
